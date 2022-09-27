bcd98a83cafb1e78da6e74925648dd090246df83
intermx-srinath <srinath@agiratech.com>
2022-09-27 17:21:53 +0530

    CRUD

--- config/roles.js
+++ config/roles.js
@@ -1,5 +1,6 @@
 module.exports={
     userCreate:{module:'user',action:'create'},
     userView:{module:'user',action:'view'},
+    userEdit:{module:'user',action:'edit'},
     userDelete:{moule:'user',action:'Delete'}
 }
